5d307a69f9e1549a62560debfa2f9a37574b05a7
Haziq Nordin <hbnordin2@gmail.com>
2018-09-09 15:03:22 +1000

    Added API to access Dynamo Table

---
+++ helloworld/Dynamo/dynamo.py
@@ -0,0 +1,11 @@
+import boto3
+
+class DynamoTable:
+    def __init__(self, tableName):
+        self.tableName = tableName
+
+        dynamodb = boto3.resource('dynamodb')
+        self.table = dynamodb.Table(tableName)
+
+    def getEverything(self):
+        return self.table.scan()
--- helloworld/application.py
+++ helloworld/application.py
@@ -1,6 +1,8 @@
 #!flask/bin/python
 import json
 from flask import Flask, Response
+
+from Dynamo.dynamo import DynamoTable
 from helloworld.flaskrun import flaskrun
 
 application = Flask(__name__)
@@ -12,6 +14,12 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+@application.route('/<topic>', methods=['GET'])
+def getCardWithTopic(topic):
+    flashCardTable = DynamoTable('Cards')
+    return Response(json.dumps({'Topic': flashCardTable.getEverything()}), mimetype='application/json', status=200)
+
 
 if __name__ == '__main__':
     flaskrun(application)
